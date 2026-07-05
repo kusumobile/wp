7638b817d2abc8503409a9f24646256ee5e3a07d
楠元 遼太郎 <m242779@axio.local>
2026-07-05 14:23:04 +0900

    Add missing first_non_empty_env to bootstrap script

--- docker-entrypoint-custom.sh
+++ docker-entrypoint-custom.sh
@@ -57,6 +57,18 @@ first_non_empty_env() {
     printf '%s' ""
 }
 
+first_non_empty_env() {
+    for var_name in "$@"; do
+        eval var_value="\${${var_name}:-}"
+        if [ -n "$var_value" ]; then
+            printf '%s' "$var_value"
+            return 0
+        fi
+    done
+
+    printf '%s' ""
+}
+
 escape_sed_replacement() {
     printf '%s' "$1" | sed -e 's/[\/&\\]/\\&/g'
 }
